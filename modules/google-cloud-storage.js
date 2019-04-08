a857f0b79ebf5ef542ff76438458a0d3755ffb53
Shane Riegodedios <sriegodedios@ksu-wireless-83-18.campus.ksu.edu>
2019-04-08 14:10:23 -0500

    Google Cloud Support

---
+++ modules/google-cloud-storage.js
@@ -0,0 +1,34 @@
+const GoogleCloudStorage = require('@google-cloud/storage');
+  
+  const GOOGLE_CLOUD_PROJECT_ID = 'gcs-demo-123456'; // Replace with your project ID
+  const GOOGLE_CLOUD_KEYFILE = 'path-to-the-private-key'; // Replace with the path to the downloaded private key
+  
+  const storage = GoogleCloudStorage({
+    projectId: GOOGLE_CLOUD_PROJECT_ID,
+    keyFilename: GOOGLE_CLOUD_KEYFILE,
+  });
+
+
+
+
+  exports.getPublicUrl = (bucketName, fileName) => `https://storage.googleapis.com/${bucketName}/${fileName}`;
+  /**
+   * Copy file from local to a GCS bucket.
+   * Uploaded file will be made publicly accessible.
+   *
+   * @param {string} localFilePath
+   * @param {string} bucketName
+   * @param {Object} [options]
+   * @return {Promise.<string>} - The public URL of the uploaded file.
+   */
+  exports.copyFileToGCS = (localFilePath, bucketName, options) => {
+    options = options || {};
+  
+    const bucket = storage.bucket(bucketName);
+    const fileName = path.basename(localFilePath);
+    const file = bucket.file(fileName);
+  
+    return bucket.upload(localFilePath, options)
+      .then(() => file.makePublic())
+      .then(() => exports.getPublicUrl(bucketName, gcsName));
+  };
