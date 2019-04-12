ae99b7578263cd2e771137fbd24495474a5a425d
Shane Riegodedios <sriegodedios@Shanes-MacBook-Pro.local>
2019-04-12 00:18:14 -0500

    Added homepage builder

--- modules/homePageVideo.js
+++ modules/homePageVideo.js
@@ -21,15 +21,38 @@ function ConstructHomePage(req, res)
 {
   console.log("IN THE RENDER")
 
-    var sql ="SELECT V.VideoId, V.UserId, A.Username, V.Title, CloudLink FROM `Videos` V INNER JOIN `Accounts` A ON V.UserId=A.ID"
-    con.query(sql, function (err, result) {
-        if (err) throw err;
+   //var sql ="SELECT V.VideoId, V.UserId, A.Username, V.Title, CloudLink FROM `Videos` V INNER JOIN `Accounts` A ON V.UserId=A.ID"
+   // con.query(sql, function (err, result) {
+   //     if (err) throw err;
          // console.log("IN THE RENDER")
-          //req.session.homepage = result
+         //req.session.homepage = result
          // res.render('pages/home',{title: 'Home', videos: result});
-         return res.send(result)
+   //      return res.send(result)
 
-    });
+   // });
+
+    var query = connection.query('SELECT * FROM posts');
+    query
+      .on('error', function(err) {
+        // Handle error, an 'end' event will be emitted after this as well
+        throw err
+      })
+      .on('fields', function(fields) {
+        // the field packets for the rows to follow
+      })
+      .on('result', function(row) {
+        // Pausing the connnection is useful if your processing involves I/O
+        connection.pause();
+
+        processRow(row, function() {
+          connection.resume();
+        });
+      })
+      .on('end', function(result) {
+        // all rows have been received
+        return res.send(result)
+
+      });
 
 
 
